07df11a0e055d2072197ef63f11183e5338068f1
adam-stone <25stonea@charlottelatin.org>
2023-11-22 12:52:55 -0500

    loading screen fixed; more pages added

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,3 +1,6 @@
+let allTimeouts = [];
+let loadingScreen;
+
 window.onload = () => {
 
 const body = document.getElementsByTagName("body")[0];
@@ -8,7 +11,14 @@ const name2 = document.getElementById("name-2");
 let rotating = false;
 let stopRotating = false;
 
+loadingScreen = document.getElementById("loading-screen");
+
+let rotationCount = 0;
 const rotationFunc = (loadingInterval) => {
+    rotationCount += 1;
+    if (rotationCount > 4) {
+        endLoading();
+    }
     if (stopRotating){
         clearInterval(loadingInterval);
         return;
@@ -16,28 +26,47 @@ const rotationFunc = (loadingInterval) => {
     rotating = true;
     name1.style.animation = "2s ease loading-rotate-1"
     name2.style.animation = "2s ease loading-rotate-2"
-    setTimeout(() => {rotating = false; name1.style.animation = "none"; name2.style.animation = "none";}, 2000);
+    allTimeouts.push(setTimeout(() => {rotating = false; name1.style.animation = "none"; name2.style.animation = "none";}, 2000));
 };
 
 setTimeout(() => {
     const loadingInterval = setInterval(() => {
         rotationFunc(loadingInterval);
     }, 2500);
+    allTimeouts.push(loadingInterval)
     rotationFunc();
 }, 100);
 
-document.getElementById("background-video").addEventListener("canplay", (event) => {
-    setTimeout(() => {
+const endLoading = (event) => {
+    //setTimeout(() => {
         stopRotating = true;
-        setTimeout(() => {
+        allTimeouts.push(setTimeout(() => {
             console.log("triggering");
-            const loadingScreen = document.getElementById("loading-screen");
             console.log(loadingScreen)
             loadingScreen.style.animation = "1s ease-out fade-out forwards";
             name1.style.animation = "1s ease-out fade-to-black-1 forwards";
             name2.style.animation = "1s ease-out fade-to-black-2 forwards";
-            setTimeout(() => {body.style.overflowY = "scroll";}, 1000);
-        }, 2000);
-    },1000);
-});
-}
+            setTimeout(() => {loadingScreen.style.display = "none"; body.style.overflowY = "scroll";}, 1000);
+        }, 2000));
+    //},1000);
+};
+
+document.getElementById("background-video").addEventListener("canplay", endLoading);
+};
+
+window.onunload = () => {
+    allTimeouts.forEach(t => {
+        try {
+            clearInterval(t);
+        }
+        catch {
+
+        }
+        try {
+            clearTimeout(t);
+        }
+        catch {
+
+        }
+    });
+};
